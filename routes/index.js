d2d4c96486d40f4a442db182feb9409bcf44a6f4
Kumar (EXT) <nilesh525.kmr@gmail.com>
2019-12-27 16:47:03 +0530

    first commit dynamodb

--- routes/index.js
+++ routes/index.js
@@ -132,4 +132,124 @@ router.post('/register', function(req, res, next) {
  
 });
 
+//reset password
+router.get('/resetpass.html', function(req, res, next) {
+  res.render('resetpass.html', { title: 'reset password' });
+});
+
+router.post('/resetpass',function(req,res,next){
+  var name = req.body.lastname;
+  var dob =req.body.date;
+
+  var awsConfigInfo = {
+    region: 'ap-south-1',
+    identityPoolId: 'ap-south-1:c49a9371-0b0b-4359-b111-138b5d17e4af',
+  }
+
+  var awsConfig = awsConfigInfo;
+  var REGION = awsConfig.region;
+  var identityPoolId = awsConfig.identityPoolId;
+  AWS.config.update({
+    region:REGION
+  });
+  AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+      IdentityPoolId:identityPoolId 
+  });
+  console.log('inside reset pass....'+'lastname '+name+' dob '+dob)
+
+  var params = {
+    ExpressionAttributeValues: {
+      ':u': {S: name},
+      ':d' : {S: dob},
+    },
+    ExpressionAttributeNames: {
+      "#lastname": "lastname",
+      "#date":"date"
+    },
+    //KeyConditionExpression: 'username = :u',
+    ProjectionExpression: 'lastname, pass',
+    //FilterExpression: 'contains (:u, :d)',
+    FilterExpression: '#lastname = :u AND #date=:d',
+    TableName: 'users'
+  };
+
+   var dynamodb = new AWS.DynamoDB();
+   dynamodb.scan(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else{
+      
+      if(data.Items.length==0){
+        res.render('error.html', { title: 'Invalid username or DOB' });
+      }else{
+        var result ='';
+        data.Items.forEach(function(itemdata) {
+          result=itemdata.lastname.S;
+          console.log("Item :",itemdata.lastname.S);
+       });
+       console.log(result+'---------------------'); 
+        res.render('resetpage.html',{ title : result});
+      }
+    }   
+   });
+});
+
+router.post('/newpass',function(req,res,next){
+  console.log('inside new passss')
+  var pass = req.body.pass;
+  var cpass= req.body.cpass;
+  var name =req.body.name;
+
+  var awsConfigInfo = {
+    region: 'ap-south-1',
+    identityPoolId: 'ap-south-1:c49a9371-0b0b-4359-b111-138b5d17e4af',
+  }
+
+  var awsConfig = awsConfigInfo;
+  var REGION = awsConfig.region;
+  var identityPoolId = awsConfig.identityPoolId;
+  AWS.config.update({
+    region:REGION
+  });
+  AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+      IdentityPoolId:identityPoolId 
+  });
+  var dynamodb = new AWS.DynamoDB();
+  var params = {
+    ExpressionAttributeNames: {
+     "#P": "pass", 
+    }, 
+    ExpressionAttributeValues: {
+     ":t": {
+       S: pass
+      }, 
+    }, 
+    Key: {
+     "lastname": {
+       S: name
+      }, 
+    }, 
+    ReturnValues: "ALL_NEW", 
+    TableName: "users", 
+    UpdateExpression: "SET #P = :t"
+   };
+
+   console.log('inside new passss '+name+' '+pass+' '+cpass)
+
+   if(pass==cpass){
+    console.log('inside new passss===passss')
+    dynamodb.updateItem(params, function(err, data) {
+      if (err){
+        console.log(err, err.stack);
+        res.render('error.html', { title: 'resources din not found ...' });
+      }  // an error occurred
+      else {
+        console.log(data);  
+        var result = data.Attributes.pass.S;
+        console.log(data.Attributes.pass.S);
+        res.render('successupdate.html', { title: result });
+      }          // successful response
+    })
+   }
+})
+
 module.exports = router;
